d76b9f15515eaac2c7cd7e961ff8e8759ceb3802
Ho-oDiNi <78462812+Ho-oDiNi@users.noreply.github.com>
2022-03-29 00:24:52 +0700

    add full

--- curs.cpp
+++ curs.cpp
@@ -5,6 +5,9 @@
 #include <algorithm>
 #include <string>
 #include <cstdlib>
+#include <unordered_map>
+#include <vector>
+
 
 using namespace std;
 const int N = 4000;
@@ -307,6 +310,99 @@ void tree(record** arr, int k)
 	rmtree(root);
 }
 
+void shannon(const int n, double p[], int Length[], char c[][20]) {
+    double q[n];
+    q[0] = 0;
+    Length[0] = -floor(log2(p[0]));
+    for (int i = 1; i < n; ++i) {
+        q[i] = q[i - 1] + p[i - 1];
+        Length[i] = -floor(log2(p[i]));
+    }
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 0; j < Length[i - 1]; ++j) {
+            q[i - 1] *= 2;
+            c[i - 1][j] = floor(q[i - 1]) + '0';
+            if (q[i - 1] >= 1) {
+                q[i - 1] -= 1;
+            }
+        }
+
+    }
+}
+
+unordered_map<char, int> get_char_counts_from_file(const string &file_name, int &file_size, int n = N) {
+    ifstream file(file_name, ios::binary);
+    if (!file.is_open()) {
+        throw runtime_error("Could not open file");
+    }
+    char ch_arr[sizeof(record) * n];
+    file.read((char *) ch_arr, sizeof(record) * n);
+    file.close();
+
+    unordered_map<char, int> counter_map;
+    file_size = 0;
+    for (auto ch : ch_arr) {
+        counter_map[ch]++;
+        file_size++;
+    }
+    return counter_map;
+}
+
+vector<pair<double, char>> calc_probabilities(const unordered_map<char, int> &counter_map, int count) {
+    vector<pair<double, char>> probabilities;
+    probabilities.reserve(counter_map.size());
+    for (auto i : counter_map) {
+        probabilities.emplace_back(make_pair((double) i.second / count, i.first));
+    }
+    return probabilities;
+}
+
+void coding() {
+    int file_size;
+    unordered_map<char, int> counter_map;
+    counter_map = get_char_counts_from_file("testBase1.dat", file_size);
+
+    auto probabilities = calc_probabilities(counter_map, file_size);
+    counter_map.clear();
+
+    sort(probabilities.begin(), probabilities.end(), greater<pair<double, char>>());
+    cout << "Probabil.  char\n";
+    for (auto i : probabilities) {
+        cout << fixed << i.first << " | " << i.second << '\n';
+    }
+
+    const int n = (int) probabilities.size();
+
+    auto c = new char[n][20];
+    auto Length = new int[n];
+    auto p = new double[n];
+    for (int i = 0; i < n; ++i) {
+        p[i] = probabilities[i].first;
+    }
+
+    double shen = 0;
+    shannon(n, p, Length, c);
+    cout << "\nShannon Code:\n";
+    cout << "\nCh  Prob      Code\n";
+    double avg_len = 0;
+    double entropy = 0;
+    for (int i = 0; i < n; i++) {
+        avg_len += Length[i] * p[i];
+        entropy -= p[i] * log2(p[i]);
+        printf("%c | %.5lf | ", probabilities[i].second, p[i]);
+        for (int j = 0; j < Length[i]; ++j) {
+            printf("%c", c[i][j]);
+        }
+        cout << '\n';
+        shen += p[i];
+    }
+    cout << "Average length = " << avg_len << '\n'
+         << "Entropy = " << entropy << '\n'
+         << "Average length < entropy + 1\n"
+         << "n = " << n << endl;
+
+}
+
 int main()
 {
 	int n = 4000, temp, ind, k = -1;
@@ -348,7 +444,8 @@ int main()
 				tree(&A[ind], k);
 			break;
 		case 5:
-			coding(n);
+			coding();
+			system("pause");
             break;	
 		default:
 			return 1;
